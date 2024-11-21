2c9b08d31c60fcd4f3ab33a025d8edfa5316db83
Philippe Menard <phil.menard1668@gmail.com>
2024-11-21 11:34:01 +0100

    removed unnecessary protos

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 12:14:38 by pmenard           #+#    #+#             */
-/*   Updated: 2024/11/21 11:12:53 by pmenard          ###   ########.fr       */
+/*   Updated: 2024/11/21 11:30:06 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,6 @@ void	ft_putnum(int nb, int *result);
 void	ft_put_unum(unsigned int nb, int *result);
 void	ft_puthex(unsigned int x, int *result, char c);
 size_t	len_int(unsigned int n);
-char	*ft_itoa(unsigned int n);
-char	*ft_itohex(unsigned int x, int loworup);
 int		ft_strlen(char *s);
 
 #endif
